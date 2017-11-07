eac0908a0a4cb40c56cec865114a90b2ebeddf6c
digitopoi <hubo85@gmail.com>
2017-11-07 12:19:50 -0600

    Add create post method on notes controller

--- ElevenNote.Web/Controllers/NotesController.cs
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,6 +18,17 @@ namespace ElevenNote.Web.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
