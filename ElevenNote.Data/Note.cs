8ab760572e4c63878685afb7e4820cc9acf5c754
digitopoi <hubo85@gmail.com>
2017-11-07 10:32:48 -0600

    Add note model

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        public int NoteId { get; set; }
+        public Guid OwnerId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset ModifiedUtc { get; set; }
+    }
+}
