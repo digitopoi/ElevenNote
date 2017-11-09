7dd2a1ec6352c10774901292e4e2c47abacb5374
digitopoi <hubo85@gmail.com>
2017-11-08 22:31:46 -0600

    finish module 4

--- ElevenNote.Web/Controllers/NoteController.cs
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,15 @@ using System.Web.Mvc;
 
 namespace ElevenNote.Web.Controllers
 {
+    [Authorize]
     public class NoteController : Controller
     {
         // GET: Note
         public ActionResult Index()
         {
-            return View();
+            var model = new NoteListItem[0];
+
+            return View(model);
         }
     }
 }
