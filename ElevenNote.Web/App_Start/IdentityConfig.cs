b9f0dc7db76157f2cdd874f61ba45f21049e9d75
digitopoi <hubo85@gmail.com>
2017-11-15 21:50:18 -0600

    Initialize database

--- ElevenNote.Web/App_Start/IdentityConfig.cs
+++ ElevenNote.Web/App_Start/IdentityConfig.cs
@@ -42,7 +42,7 @@ namespace ElevenNote.Data
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ElevenNoteUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(new UserStore<ElevenNoteUser>(context.Get<ElevenNoteDbContext>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ElevenNoteUser>(manager)
             {
